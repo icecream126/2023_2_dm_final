b011c7d693fc8c68fd1da2e5f0d7e5099a591fab
icecream126 <kim123hyomin@gmail.com>
2023-12-07 23:20:25 +0900

    oversample and add train.sh

--- train.sh
+++ train.sh
@@ -13,3 +13,8 @@
 # CUDA_VISIBLE_DEVICES=1 python src_han/main.py --label_num 5 --data_type glove_tk --dataset_dir eq_dataset
 
 CUDA_VISIBLE_DEVICES=0 python src/main.py --label_num 10 --data_type tfidf
+
+CUDA_VISIBLE_DEVICES=1 python src_gat/main.py --data_aug oversample --feature_dim 500 --lr 0.0001
+
+CUDA_VISIBLE_DEVICES=2 python src_gat/main.py --data_aug oversample --feature_dim 500 --lr 0.001
+CUDA_VISIBLE_DEVICES=3 python src_gat/main.py --data_aug oversample --feature_dim 500 --lr 0.005
